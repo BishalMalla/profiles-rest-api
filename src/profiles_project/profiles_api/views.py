ed5540cd7405e3ca20afb6ff28340a77fd6229dd
Bishal Malla <lxixchg@gmail.com>
2018-01-24 11:20:28 -0700

    added all the aspect of viewset

--- src/profiles_project/profiles_api/views.py
+++ src/profiles_project/profiles_api/views.py
@@ -53,6 +53,8 @@ class HelloApiView(APIView):
 class HelloViewSet(viewsets.ViewSet):
     """Test API ViewSet"""
 
+    serializer_class = serializers.HelloSerializer
+
     def list(self, request):
         """Return a hello message."""
 
@@ -63,3 +65,33 @@ class HelloViewSet(viewsets.ViewSet):
         ]
 
         return Response({'message': 'Hello!', 'a_viewset': a_viewset})
+
+    def create(self,request):
+        """Create a new hello message"""
+
+        serializer = serializers.HelloSerializer(data=request.data)
+
+        if serializer.is_valid():
+            name = serializer.data.get('name')
+            message = 'Hello {0}'.format(name)
+
+            return Response({'message':message})
+
+        else:
+            return Response(serializer.errors, status= status.HTTP_400_BAD_REQUEST)
+
+    def retrieve(self,request, pk=None):
+        """retrieves a particular obj by ID """
+        return Response({'httpmethod': 'GET'})
+
+    def update(self,request, pk=None):
+        """Handles updating an object"""
+        return Response({'httpmethod':'PUT'})
+
+    def partialUpdate(self,request,pk=None):
+        """Hanldles partialUpdate"""
+        return Response({'httpmethod':'PATCH'})
+
+    def destroy(self,request,pk=None):
+        """hanldes removing an Obj"""
+        return Response({'httpmethod':'DELETE'})
